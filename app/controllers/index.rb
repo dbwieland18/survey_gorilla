36b01eb38328a8259604fa9a8a1b2d3cfd0f1a0b
Batman <Bruce@wayne.enterprises.com>
2014-03-21 15:27:58 -0500

    tweaks

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,12 +1,12 @@
 get '/' do
   # Look in app/views/index.erb
-  erb :index
-  # if session[:user_id].nil?
-  # 	redirect to "/sessions/new"
-  # else
-  # 	@user = User.find(session[:user_id])
-  # 	erb :index
-  # end
+  # erb :index
+  if session[:user_id].nil?
+  	redirect to "/sessions/new"
+  else
+  	@user = User.find(session[:user_id])
+  	erb :index
+  end
 end
 
 
