23425ae493eefd62dfe98ac3598260bc59962290
Tony has a clown penis <batman@devbootcamp.com>
2014-03-21 16:26:58 -0500

    added survey creation to controller

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -4,3 +4,20 @@ get '/surveys' do
 	erb  :"surveys/index"
 
 end
+
+# Show the form to create a new survey (new)
+get "/surveys/new" do
+  erb :"surveys/new"
+  erb :"questions/new"
+  erb :"possible_choices/new"
+end
+
+# Create a new survey (create)
+post "/surveys" do
+  @survey = Survey.new(params)
+  if @survey.save
+    redirect to("/surveys/#{@survey.id}")
+  else
+    erb :"surveys/new"
+  end
+end
