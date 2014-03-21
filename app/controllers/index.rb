262b3a0ff986b0ec325e7b285ce83d4a408566ef
Batman <Bruce@wayne.enterprises.com>
2014-03-21 15:25:12 -0500

    some view stuff

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,11 +1,12 @@
 get '/' do
   # Look in app/views/index.erb
-  if session[:user_id].nil?
-  	redirect to "/sessions/new"
-  else
-  	@user = User.find(session[:user_id])
-  	erb :index
-  end
+  erb :index
+  # if session[:user_id].nil?
+  # 	redirect to "/sessions/new"
+  # else
+  # 	@user = User.find(session[:user_id])
+  # 	erb :index
+  # end
 end
 
 
